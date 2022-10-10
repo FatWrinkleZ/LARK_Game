2e3001bfbf1c0989b28d26bf6ac87adf97fa5076
FatWrinkleZ <mgiraldo2003@gmail.com>
2022-10-10 18:19:07 -0400

    small update

--- C_Code/MikeEngine/Engine.h
+++ C_Code/MikeEngine/Engine.h
@@ -3,6 +3,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#ifdef __linux
+#include <unistd.h>
+#endif
 
 int PLAYING;
 
--- LARK.sh
+++ LARK.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "Welcome to the LARK Game"
+./C_Code/LARK_GAME.out
 
