81c14d968b49f2ace8b7624cf4ad3a9ddc21871f
FatWrinkleZ <40575375+FatWrinkleZ@users.noreply.github.com>
2022-10-20 20:00:16 -0400

    Update compile_game.sh

--- C_Code/compile_game.sh
+++ C_Code/compile_game.sh
@@ -3,7 +3,7 @@ echo 'Compiling game...'
 rm LARK_GAME.out 2>/dev/null
 rm core.* 2>/dev/null
 #gnu prog   our libraries      Engine library     math      output       errors
-gcc main.c LARK_Libraries/*.c MikeEngine/Engine.c -lm -o LARK_GAME.out 2>errors.log
+gcc main.c LARK_Libraries/*.c MikeEngine/Engine.c -lm -o LARK_GAME.out -std=c99 2>errors.log
 
 ls LARK_GAME.out 2>/dev/null 1>/dev/null && echo 'Game Compiled Successfuly' && rm errors.log 2>/dev/null && chmod 744 LARK_GAME.out
 ls LARK_GAME.out 2>/dev/null 1>/dev/null || echo 'ERROR! Game did not compile! Check [errors.log]'
