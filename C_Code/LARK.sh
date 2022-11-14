3a40b3d8d836642f33bbf5cc2ae50118fc8bf8b5
FatWrinkleZ <40575375+FatWrinkleZ@users.noreply.github.com>
2022-11-14 15:15:51 -0500

    made the compile script hidden. now run ./LARK.sh to compile and run

---
+++ C_Code/LARK.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Welcome to the LARK Game"
+./.compile_game.sh && ./LARK_GAME.out
+
--- LARK.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Welcome to the LARK Game"
-./C_Code/compile_game.sh && ./C_Code/LARK_GAME.out
-
